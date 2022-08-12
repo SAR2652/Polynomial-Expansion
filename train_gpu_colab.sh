424b720a7195382ec785fa3b1247d723e687b6a2
Sarvesh Relekar <sr5796@nyu.edu>
2022-08-12 02:07:06 -0400

    Fixed Training Error

--- train_gpu_colab.sh
+++ train_gpu_colab.sh
@@ -1,1 +1,1 @@
-python train.py ./data/train.txt --accelerator cuda --hidden_size 320 --model_path /content/drive/MyDrive/new_encoder_decoder_model.pt --tokenizer_filepath ./tokenizers/tokenizer.pickle
+python train.py ./data/training.csv --accelerator cuda --hidden_size 320 --model_path /content/drive/MyDrive/new_encoder_decoder_model.pt --tokenizer_filepath ./tokenizers/tokenizer.pickle
