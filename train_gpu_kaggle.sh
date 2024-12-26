008914efc42f41e28d310ea7937df6dd2c7b58b7
Sarvesh Relekar <sar265278@gmail.com>
2024-12-26 23:28:14 +0530

    Changed Output Directory

--- train_gpu_kaggle.sh
+++ train_gpu_kaggle.sh
@@ -1,4 +1,4 @@
 python -m jax_implementation.train \
     --input_filepath /kaggle/input/polynomialexpansion/training.csv \
-    --output_dir /kaggle/working \
+    --output_dir /kaggle/working/output \
     --tokenizer_filepath /kaggle/input/polynomialexpansion/tokenizer.joblib
