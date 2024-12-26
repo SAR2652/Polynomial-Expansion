8e6b5c42f4ce8da6144bae357c873b5c884d79c1
Sarvesh Relekar <sar265278@gmail.com>
2024-12-26 17:36:48 +0530

    Finalized JAX Model Training

--- train_gpu_colab.sh
+++ train_gpu_colab.sh
@@ -1,2 +1,2 @@
-python -m jax_implementation.train --input_filepath /content/drive/MyDrive/polynomial_expansion/training.csv --output_dir /content/drive/MyDrive/polynomial_expansion/output --tokenizer_filepath /content/drive/MyDrive/polynomial_expansion/output/tokenizer.joblib
+python -m jax_implementation.train --input_filepath /content/drive/MyDrive/polynomial_expansion/output/training.csv --output_dir /content/drive/MyDrive/polynomial_expansion/output --tokenizer_filepath /content/drive/MyDrive/polynomial_expansion/output/tokenizer.joblib
 # python train.py ./data/training.csv --accelerator cuda --hidden_size 320 --model_path /content/drive/MyDrive/new_encoder_decoder_model.pt --tokenizer_filepath ./tokenizers/tokenizer.pickle --epochs 200
