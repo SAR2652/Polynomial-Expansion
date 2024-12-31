9eabb394826b1e09e0059103e661dc9963efa4fa
Sarvesh Relekar <sar265278@gmail.com>
2024-12-31 17:08:37 +0530

    Continue Model Training

--- train_gpu_colab.sh
+++ train_gpu_colab.sh
@@ -6,9 +6,9 @@ python -m pytorch_new_implementation.train \
     --embed_dim 64 \
     --epochs 250 \
     --batch_size 768 \
-    --learning_rate 5e-5 \
+    --learning_rate 1e-6 \
     --continue_from_ckpt \
-    --ckpt_file /content/drive/MyDrive/polynomial_expansion/output/best_model_250.pth
+    --ckpt_file /content/drive/MyDrive/polynomial_expansion/output/best_model_272.pth
 
 # python -m jax_implementation.train --input_filepath /content/drive/MyDrive/polynomial_expansion/output/training.csv --output_dir /content/drive/MyDrive/polynomial_expansion/output --tokenizer_filepath /content/drive/MyDrive/polynomial_expansion/output/tokenizer.joblib
 # python train.py ./data/training.csv --accelerator cuda --hidden_size 320 --model_path /content/drive/MyDrive/new_encoder_decoder_model.pt --tokenizer_filepath ./tokenizers/tokenizer.pickle --epochs 200
