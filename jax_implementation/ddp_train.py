a1acf35482306efb00143a4a5cd5365d3452b4db
Sarvesh Relekar <sr5796@nyu.edu>
2025-03-13 03:17:41 -0400

    Model Training Check

--- jax_implementation/ddp_train.py
+++ jax_implementation/ddp_train.py
@@ -101,7 +101,7 @@ def train_step(state: train_state.TrainState, inputs: jnp.ndarray,
         logits = state.apply_fn({'params': params}, inputs)
         loss = optax.softmax_cross_entropy_with_integer_labels(logits,
                                                                targets)
-        return loss, logits
+        return loss.mean(), logits
 
     gradient_fn = jax.value_and_grad(loss_fn, has_aux=True)
     (loss, _), grads = gradient_fn(state.params)
@@ -186,6 +186,8 @@ def train_model(args):
             state, loss, grads = train_step(state, inputs, targets)
             state = update_model(state, grads)
 
+            print(loss)
+            exit(0)
             running_loss += loss.mean().item()
             if (i + 1) % (len(train_dataloader) // 100) == 0:
                 print(f'Running Loss after {i + 1} batches = '
