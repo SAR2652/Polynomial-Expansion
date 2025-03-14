167c5c63877f60beca5ed739c14954f2e8db39a0
Sarvesh Relekar <sr5796@nyu.edu>
2025-03-13 22:35:09 -0400

    Batched Inference

--- inference.sh
+++ inference.sh
@@ -1,5 +1,6 @@
 # python -m pytorch_new_implementation.batched_inference --bidirectional
 python -m jax_implementation.batched_inference \
+    --input_filepath /content/validation.csv \
     --tokenizer_filepath /content/tokenizer.joblib \
     --hidden_dim 64 \
     --embed_dim 64 \
