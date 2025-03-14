59f405f42f97392fea071391c537f6d3a29c3a69
Sarvesh Relekar <sr5796@nyu.edu>
2025-03-13 22:32:49 -0400

    Batched Inference

--- inference.sh
+++ inference.sh
@@ -1,2 +1,7 @@
 # python -m pytorch_new_implementation.batched_inference --bidirectional
-python -m jax_implementation.batched_inference --bidirectional
+python -m jax_implementation.batched_inference
+    --tokenizer_filepath /content/tokenizer.joblib \
+    --hidden_dim 64 \
+    --embed_dim 64 \
+    --bidirectional \
+    --ckpt_dir ./best_model_saca_bidirect_217
