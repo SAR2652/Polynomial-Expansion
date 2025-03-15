695bb0a3461fef6a85ad4ccfffcfc9d002ee6584
Sarvesh Relekar <sr5796@nyu.edu>
2025-03-14 21:21:42 -0400

    Batched Inference

--- jax_implementation/batched_inference.py
+++ jax_implementation/batched_inference.py
@@ -84,10 +84,11 @@ def batched_inference(args):
     dummy_inputs = jnp.ones((batch_size, tokenizer.MAX_SEQUENCE_LENGTH),
                             dtype=jnp.int32)
 
-    og_params = model.init(key, dummy_inputs)['params']
+    params = model.init(key, dummy_inputs)['params']
+    og_params = params.copy()
 
     # Restore checkpoint
-    state = checkpoints.restore_checkpoint(ckpt_dir, {'params': og_params})
+    state = checkpoints.restore_checkpoint(ckpt_dir, {'params': params})
     params = state['params']
 
     print(pytree_equal(og_params, params))
