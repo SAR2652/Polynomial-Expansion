bb9974f6028cf25e9dafc2f075a1d2da303fe878
Sarvesh Relekar <sr5796@nyu.edu>
2025-04-06 01:06:43 -0400

    Addedd param dtype checker

---
+++ jax_implementation/check_param_dtypes.py
@@ -0,0 +1,40 @@
+import os
+import jax
+import argparse
+from orbax.checkpoint import PyTreeCheckpointer, CheckpointManager
+
+
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--ckpt_dir',
+                        help='Model checkpoint filepath',
+                        type=str, default='./output/checkpoints')
+    return parser.parse_args()
+
+
+def print_param_dtypes(params):
+    for path, value in jax.tree_util.tree_flatten_with_path(params)[0]:
+        key = "/".join(str(p) for p in path)
+        print(f"{key}: dtype={value.dtype}, shape={value.shape}")
+
+
+def display_params(args):
+
+    jax.default_device("cpu")
+    ckpt_dir = os.path.abspath(args.ckpt_dir)
+    orbax_checkpointer = PyTreeCheckpointer()
+    checkpoint_manager = CheckpointManager(ckpt_dir, orbax_checkpointer)
+    step = checkpoint_manager.latest_step()
+    checkpoint = checkpoint_manager.restore(step)
+    state = checkpoint['state']
+    params = state['params']
+    print_param_dtypes(params)
+
+
+def main():
+    args = get_arguments()
+    display_params(args)
+
+
+if __name__ == '__main__':
+    main()
