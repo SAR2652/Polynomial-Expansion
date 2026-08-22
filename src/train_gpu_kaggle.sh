763e283078535aae28c80b4018ceeb1b3c6edd2c
Sarvesh Relekar <sar265278@gmail.com>
2026-08-22 14:48:27 -0400

    Increased embed dim and hidden dim to 128

--- src/train_gpu_kaggle.sh
+++ src/train_gpu_kaggle.sh
@@ -14,8 +14,8 @@ python -m src.jax_implementation.ddp_train_lr_schedule \
     --input_dir /kaggle/input/polynomialexpansion \
     --output_dir /kaggle/working/output \
     --tokenizer_filepath /kaggle/input/polynomialexpansion/tokenizer.joblib \
-    --hidden_dim 64 \
-    --embed_dim 64 \
+    --hidden_dim 128 \
+    --embed_dim 128 \
     --epochs 500 \
     --batch_size 1024 \
     --learning_rate 1e-4 \
